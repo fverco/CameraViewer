d5d0b0f871164f08cc279da29ee170a0c13d1705
fverco <francov2468@gmail.com>
2022-04-21 00:06:03 +0200

    Add Camera class and LibVLC global variable

---
+++ CameraViewer/Types/Camera.cs
@@ -0,0 +1,74 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+
+namespace CameraViewer.Types
+{
+    /// <summary>
+    /// A class for setting up cameras to view in the window.
+    /// </summary>
+    internal class Camera
+    {
+        /// <summary>
+        /// The display name of the camera.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The entire connection string for connecting to the camera stream.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// The media player used by VLC to play the stream.
+        /// </summary>
+        MediaPlayer VlcPlayer { get; }
+
+        /// <summary>
+        /// The view used to display the stream.
+        /// </summary>
+        VideoView VidView { get; }
+
+        /// <summary>
+        /// The Camera class constructor.
+        /// </summary>
+        /// <param name="name">The display name of the camera.</param>
+        /// <param name="conString">The connection string for the stream.</param>
+        public Camera(string name, string conString)
+        {
+            Name = name;
+            ConnectionString = conString;
+            VlcPlayer = new MediaPlayer(Globals.CameraLibVLC);
+            VidView = new VideoView()
+            {
+                MediaPlayer = VlcPlayer,
+                MinHeight = 200,
+                MinWidth = 200
+            };
+        }
+
+        /// <summary>
+        /// Returns the view used to display the stream in the UI.
+        /// </summary>
+        /// <returns>A VideoView object.</returns>
+        public VideoView VideoView()
+        {
+            return VidView;
+        }
+
+        /// <summary>
+        /// Starts the stream.
+        /// </summary>
+        public void Play()
+        {
+            VlcPlayer.Play(new Media(Globals.CameraLibVLC, ConnectionString, FromType.FromLocation));
+        }
+
+        /// <summary>
+        /// Stops the stream.
+        /// </summary>
+        public void Stop()
+        {
+            VlcPlayer.Stop();
+        }
+    }
+}
---
+++ CameraViewer/Types/Globals.cs
@@ -0,0 +1,16 @@
+﻿using LibVLCSharp.Shared;
+
+namespace CameraViewer.Types
+{
+    /// <summary>
+    /// A class for accessing global variables in the application.
+    /// </summary>
+    public static class Globals
+    {
+        /// <summary>
+        /// The reference to the VLC library. This is needed for the VLC classes to work properly.
+        /// <para>Important: There must only be one instance of this object at all times.</para>
+        /// </summary>
+        public static readonly LibVLC CameraLibVLC = new("--reset-plugins-cache");
+    }
+}
