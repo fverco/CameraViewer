97941862b8d17ccd93b20a366aabc6b817ccd7a5
fverco <francov2468@gmail.com>
2022-04-21 00:07:21 +0200

    Create basic design of MainWindow and add camera functionality

--- CameraViewer/MainWindow.xaml.cs
+++ CameraViewer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using CameraViewer.Types;
 
 namespace CameraViewer
 {
@@ -20,9 +23,45 @@ namespace CameraViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initialize the Main Window.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            SetupCameras();
+        }
+
+        /// <summary>
+        /// Setup all the saved cameras.
+        /// </summary>
+        void SetupCameras()
+        {
+            CameraPanel.Orientation = Orientation.Horizontal;
+        }
+
+        /// <summary>
+        /// Add a new camera.
+        /// </summary>
+        /// <param name="newCamera">The new camera.</param>
+        internal void AddCamera(Camera newCamera)
+        {
+            if (newCamera != null)
+            {
+                CameraPanel.Children.Add(newCamera.VideoView());
+                newCamera.Play();
+            }
+        }
+
+        /// <summary>
+        /// Opens the New Camera window for the user to add a new camera.
+        /// </summary>
+        /// <param name="sender">The object that sent the event signal.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OpenNewCameraWindow(object sender, RoutedEventArgs e)
+        {
+            var newCamWindow = new NewCameraWindow();
+            newCamWindow.Show();
         }
     }
 }
